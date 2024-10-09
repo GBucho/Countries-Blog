5d5f4e71f886e98d76eef9620ae33fce45c36754
GBucho <161348938+GBucho@users.noreply.github.com>
2024-10-09 23:08:54 +0400

    commit six change

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -1,11 +1,20 @@
-import { ChangeEvent, FormEvent, useState } from "react";
 import "./contact.css";
 
+interface FormElements extends HTMLFormControlsCollection {
+  firstname: HTMLInputElement;
+  lastname: HTMLInputElement;
+  email: HTMLInputElement;
+  usernameInput: HTMLInputElement;
+}
+interface UsernameFormElement extends HTMLFormElement {
+  readonly elements: FormElements;
+}
+
 const Contact = () => {
-  const [firstName, setFisrtname] = useState("");
-  const [lastName, setLastname] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
+  // const [firstName, setFisrtname] = useState("");
+  // const [lastName, setLastname] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [message, setMessage] = useState("");
 
   //   const [formData, setFormData] = useState({
   //     name: "",
@@ -14,53 +23,59 @@ const Contact = () => {
   //     message: "",
   //   });
 
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    console.log(firstName, lastName, email, message);
+  const handleSubmit = (e: React.FormEvent<UsernameFormElement>) => {
     e.preventDefault();
+    console.log(
+      e.currentTarget.elements.firstname.value,
+      e.currentTarget.elements.lastname.value,
+      e.currentTarget.elements.email.value,
+      e.currentTarget.elements.usernameInput.value
+    );
   };
+
   return (
     <div className="wrapper">
       <form onSubmit={handleSubmit} className="contact">
         <input
           type="text"
           name="FirstName"
-          id="FirstName"
+          id="firstname"
           placeholder="First Name"
-          value={firstName}
-          onChange={(e: ChangeEvent<HTMLInputElement>) => {
-            setFisrtname(e.target.value);
-          }}
+          // value={firstName}
+          // onChange={(e: ChangeEvent<HTMLInputElement>) => {
+          //   setFisrtname(e.target.value);
+          // }}
         />
 
         <input
           type="text"
           name="Lastname"
-          id="Lastname"
+          id="lastname"
           placeholder="Last Name"
-          value={lastName}
-          onChange={(e: ChangeEvent<HTMLInputElement>) => {
-            setLastname(e.target.value);
-          }}
+          // value={lastName}
+          // onChange={(e: ChangeEvent<HTMLInputElement>) => {
+          //   setLastname(e.target.value);
+          // }}
         />
         <input
-          type="text"
-          name="LastName"
-          id="LastName"
+          type="email"
+          name="email"
+          id="email"
           placeholder="Email Address"
-          value={email}
-          onChange={(e: ChangeEvent<HTMLInputElement>) => {
-            setEmail(e.target.value);
-          }}
+          // value={email}
+          // onChange={(e: ChangeEvent<HTMLInputElement>) => {
+          //   setEmail(e.target.value);
+          // }}
         />
 
         <textarea
-          id=""
+          id="usernameInput"
           name="Message"
-          value={message}
           placeholder="Input Message"
-          onChange={(e: ChangeEvent<HTMLTextAreaElement>) => {
-            setMessage(e.target.value);
-          }}
+          // value={message}
+          // onChange={(e: ChangeEvent<HTMLTextAreaElement>) => {
+          //   setMessage(e.target.value);
+          // }}
         />
 
         <button className="" type="submit">
